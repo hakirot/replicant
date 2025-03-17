73073cd0c773943af78e84513871e6213a05ce23
hakirot <hackirot@proton.me>
2025-03-16 23:28:29 -0700

    setup script header

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-#TODO: compare checksums and download latest if not matching
+# -- Setup Packer Build Environment
+#TODO: compare checksums and download latest when not matching
 wget --timestamping https://geo.mirror.pkgbuild.com/iso/latest/sha256sums.txt
 wget --timestamping https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso
 head -n 1 sha256sums.txt | cut -f 1 -d " " > checksum
