1a31d0f2c092bd913d8e59ad5cc1d596f43429ee
hakirot <hackirot@proton.me>
2025-03-26 22:57:42 -0700

    start creating a plan

--- http/configure.sh
+++ http/configure.sh
@@ -8,6 +8,9 @@ sudo rmdir /etc/systemd/system/getty@tty1.service.d/
 mv $HOME/.bash_profile.bak $HOME/.bash_profile
 rm -f $HOME/configure.sh
 
+# Remove pacman lockfile if it exists
+rm -f /var/lib/pacman/db.lck
+
 # install/enable sshd (Packer test builds only)
 sudo pacman -S openssh --noconfirm
 sudo systemctl enable sshd
@@ -16,4 +19,21 @@ sudo systemctl start sshd
 # Either run the entire config script 
 # here or exit and let packer run
 
+# yay
+# firewall
+# speedrun desktop env with suckess project mods
+# grab walls.zip and install
+# sara dynamic config
+# lualine patch (not patchable until lazy is run)
+#   . /home/$USER/.local/share/nvim/lazy/lualine.nvim/lua/lualine/config.lua
+#   | > theme = '16color',
+#   | > section_separators = { left = '', right = '' },
+#   . Install 16color.lua to /home/username/.local/share/nvim/lazy/lualine.nvim/lua/lualine/themes/
+# oh-my-zsh
+# backupConfs
+# polybar
+# skps
+#   - inotifytools
+
+# launch dwm last
 # replicate.sh
--- http/replicate.sh
+++ http/replicate.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -eou pipefail
 
-# yay
-# firewall
-# speedrun desktop env with suckess project mods
-# grab walls.zip and install
-# sara dynamic config
-# lualine patch (not patchable until lazy is run)
-#   . /home/$USER/.local/share/nvim/lazy/lualine.nvim/lua/lualine/config.lua
-#   | > theme = '16color',
-#   | > section_separators = { left = '', right = '' },
-#   . Install 16color.lua to /home/username/.local/share/nvim/lazy/lualine.nvim/lua/lualine/themes/
-# oh-my-zsh
-# backupConfs
-# polybar
-# skps
-#   - inotifytools
+# This'll basically just be the presentation
 
-# launch x last
+# Compile SARA
