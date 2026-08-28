8902da1e5da254e065d8e88880f660488935a439
hakirot <hackirot@proton.me>
2026-08-27 18:55:25 -0700

    updates

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 wget -q --timestamping https://geo.mirror.pkgbuild.com/iso/latest/sha256sums.txt
 wget -q --timestamping https://geo.mirror.pkgbuild.com/iso/latest/archlinux-x86_64.iso
 head -n 1 sha256sums.txt | cut -f 1 -d " " > iso_checksum
+rm sha256sums.txt
 cat iso_checksum
